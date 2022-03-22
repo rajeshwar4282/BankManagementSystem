74a0ae9c1eb0b7a3b063fc05558ef1af3d1fa0f2
ranjit2020 <jadhavranjit2017@gmail.com>
2022-03-22 18:26:40 +0530

    added password encoder utility and other changes

--- src/main/java/com/bms/authserver/controller/RegistrationController.java
+++ src/main/java/com/bms/authserver/controller/RegistrationController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.bms.authserver.pojo.RegistrationData;
 import com.bms.authserver.pojo.ResponseData;
+import com.bms.authserver.util.CommonUtils;
 
 @RestController("/emp")
 public class RegistrationController {
@@ -15,6 +16,8 @@ public class RegistrationController {
 	
 	@PostMapping("/register")
 	public <ResponseEntity>ResponseData registerNewUser(@Valid RegistrationData registrationData, BindingResult bindingResult) {
+		
+		
 		
 		ResponseData response =  new ResponseData("username", "ststus", 200, "message");
 		return response;
--- src/test/java/com/bms/authserver/controller/RegistrationControllerTest.java
+++ src/test/java/com/bms/authserver/controller/RegistrationControllerTest.java
@@ -3,21 +3,49 @@ package com.bms.authserver.controller;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import com.bms.authserver.pojo.RegistrationData;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+@WebAppConfiguration
 class RegistrationControllerTest {
 
+	protected MockMvc mvc;
+	@Autowired
+	WebApplicationContext webApplicationContext;
+
+	protected void setUp() {
+		mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+
+	protected String mapToJson(Object obj) throws JsonProcessingException {
+		ObjectMapper objectMapper = new ObjectMapper();
+		return objectMapper.writeValueAsString(obj);
+	}
+
 	@Test
-	void registerTest1() {
-		String uri = "/register";
-		
-		RegistrationData registrationData = 
-				new RegistrationData("ranjit", "aser@987", "ranjit", "jadhav", "ion@smair.com",
-						"male", null, 9878, "ASJH1928J", "Abac chs", 
-						"mumbai", "maharashtra", "india", 400015, "loan");
-		
-		
-		
+	void registerTest1() throws Exception {
+		String uri = "/emp/register";
+
+		RegistrationData registrationData = new RegistrationData("ranjit", "aser@987", "ranjit", "jadhav",
+				"ion@smair.com", "male", null, 9878, "ASJH1928J", "Abac chs", "mumbai", "maharashtra", "india", 400015,
+				"loan");
+
+		String inputJson = mapToJson(registrationData);
+		MvcResult mvcResult = mvc.perform(
+				MockMvcRequestBuilders.post(uri).contentType(MediaType.APPLICATION_JSON_VALUE).content(inputJson))
+				.andReturn();
+
+		int status = mvcResult.getResponse().getStatus();
+
 	}
 }
